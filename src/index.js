35e0d120448be62941d15ab5a98a5cba59d4b2b7
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-06-11 23:04:48 +0400

    feat: add os info modul

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { stdin, stdout, argv } from 'process';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { createInterface } from 'readline';
+
+import { osInfo } from './utils/osInfo.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,12 +17,24 @@ const app = async () => {
 	process.stdin.setEncoding('utf8');
 
 	stdout.write(`Welcome to the File Manager, ${await userName(argv)}!\n`);
-
 	stdout.write(`You are currently in ${__dirname}\n`);
 
-	stdin.on('data', data => console.log(data));
 
-	process.on('exit', code => console.log(code));
+	const mainInterface = createInterface(stdin, stdout);
+
+	mainInterface.on('line', (line) => {
+		if (line.startsWith('os ')) {
+			osInfo(line.split('--')[1]);
+		}
+
+		console.log('You are currently in ' + process.cwd());
+	});
+
+
+
+	// stdin.on('data', data => console.log(data));
+
+	// process.on('exit', code => console.log(code));
 }
 
 await app();
