5b77258f0fdc42e0e24223aef138b7a1cdaee013
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-06-12 23:03:41 +0400

    feat: add navigation options

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { stdin, stdout, argv } from 'process';
-import path from 'path';
+import path, { format } from 'path';
 import { fileURLToPath } from 'url';
 import { createInterface } from 'readline';
 
@@ -13,6 +13,7 @@ import { copyFile } from './utils/copyFile.js';
 import { deleteFile } from './utils/deleteFile.js';
 import { compress } from './utils/compress.js';
 import { decompress } from './utils/decompress.js';
+import { setHomeDirectory, cd, up } from './utils/navigation.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,59 +26,63 @@ const userName = async (args) =>
 const app = async () => {
 	process.stdin.setEncoding('utf8');
 
-	stdout.write(`Welcome to the File Manager, ${await userName(argv)}!\n`);
-	stdout.write(`You are currently in ${__dirname}\n`);
+	const homeDirectory = setHomeDirectory();
 
+	stdout.write(`Welcome to the File Manager, ${await userName(argv)}!\n`);
 
 	const mainInterface = createInterface(stdin, stdout);
 
 	mainInterface.on('line', (line) => {
-		try {
-			if (line.startsWith('os')) {
-				osInfo(line.split('--')[1]);
-			}
 
-			if (line.startsWith('hash')) {
-				hashCalculation(line.slice(5));
-			}
-
-			if (line.startsWith('ls')) {
-				list();
-			}
-
-			if (line.startsWith('cat')) {
-				readFile(line.slice(4));
-			}
-
-			if (line.startsWith('add')) {
-				addFile(line.slice(4));
-			}
-
-			if (line.startsWith('rn')) {
-				renameFile(line.slice(3));
-			}
-
-			if (line.startsWith('cp')) {
-				copyFile(line.slice(3));
-			}
-
-			if (line.startsWith('rm')) {
-				deleteFile(line.slice(3));
-			}
-
-			if (line.startsWith('compress')) {
-				compress(line.slice(9));
-			}
-
-			if (line.startsWith('decompress')) {
-				decompress(line.slice(11));
-			}
-
-		} catch (err) {
-			console.log(err)
+		if (line.startsWith('os')) {
+			osInfo(line.split('--')[1]);
 		}
 
-		// console.log('You are currently in ' + process.cwd());
+		if (line.startsWith('hash')) {
+			hashCalculation(line.slice(5));
+		}
+
+		if (line.startsWith('ls')) {
+			list();
+		}
+
+		if (line.startsWith('cat')) {
+			readFile(line.slice(4));
+		}
+
+		if (line.startsWith('add')) {
+			addFile(line.slice(4));
+		}
+
+		if (line.startsWith('rn')) {
+			renameFile(line.slice(3));
+		}
+
+		if (line.startsWith('cp')) {
+			copyFile(line.slice(3));
+		}
+
+		if (line.startsWith('rm')) {
+			deleteFile(line.slice(3));
+		}
+
+		if (line.startsWith('compress')) {
+			compress(line.slice(9));
+		}
+
+		if (line.startsWith('decompress')) {
+			decompress(line.slice(11));
+		}
+
+		if (line === 'up') {
+			up();
+		}
+
+		if (line.startsWith('cd')) {
+			cd(line.slice(3), homeDirectory);
+		}
+
+		stdout.write('Invalid input\n');
 	});
 
 	process.on('SIGINT', () => { process.exit(); });
